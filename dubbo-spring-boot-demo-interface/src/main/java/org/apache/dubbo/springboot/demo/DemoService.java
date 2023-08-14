cc3b2655761f8d70ab906640f78d53f4cbd0a1df
zhangjunfeng <im.zhangjunfeng@qq.com>
2023-08-14 17:35:59 +0800

    feat: 初始化项目

---
+++ dubbo-spring-boot-demo-interface/src/main/java/org/apache/dubbo/springboot/demo/DemoService.java
@@ -0,0 +1,10 @@
+package org.apache.dubbo.springboot.demo;
+
+/**
+ * @author: zhangjunfeng
+ * @createTime: 2023/08/14
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+}
---
+++ dubbo-spring-boot-demo-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/ProviderApplication.java
@@ -0,0 +1,17 @@
+package org.apache.dubbo.springboot.demo.provider;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhangjunfeng
+ * @createTime: 2023/08/14
+ */
+@SpringBootApplication
+@EnableDubbo
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
